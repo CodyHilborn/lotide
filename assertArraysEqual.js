c0106f361419403e20a26802a9f2a203df25790b
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-11 01:21:58 +0000

    Linted previous code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,7 +8,7 @@ const assertArraysEqual = function(arr1, arr2) {
     }
   }
   return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2} ✅✅✅`);
-}
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 4, 3]);
