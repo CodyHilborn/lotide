188cb80d2df84433f9928e64cb5b64fc87088d93
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-11 02:31:24 +0000

    Added a function to remove items from source argument

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const assertArraysEqual = function(actual, expected) {
+  const equalArray = eqArrays(actual, expected);
+  if (equalArray) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+
+const without = function(source, itemsToRemove) {
+  const newArr = source;
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (newArr[i] === itemsToRemove[j]) {
+        newArr.splice(i, 1);
+      }
+    }
+  }
+  return newArr;
+};
+
+
+
