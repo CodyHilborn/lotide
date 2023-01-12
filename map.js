1abf47a8872e777c5a8e087b06542783c12bbaa7
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-12 20:45:00 +0000

    Added further implementation to map function

--- map.js
+++ map.js
@@ -1,10 +1,47 @@
-const words = ['welcome', 'to', 'the', 'jungle'];
 
-const map = function() {
-  const results = [];
-  return results;
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// -------------------------------------------------------------------------------------------
+
+const assertArraysEqual = function(actual, expected) {
+  const equalArray = eqArrays(actual, expected);
+  if (equalArray) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+// -------------------------------------------------------------------------------------------
+
+
+const words = ['welcome', 'to', 'the', 'jungle'];
+const numbers = [1, 2, 3, 4]
+
+const map = function(list, action) {
+  const output= [];
+  for (let item of list) {
+    output.push(action(item))
+  }
+  return output;
 };
 
 
-const results = map(words, word => word [0]);
-console.log(results);
+// const results1 = map(words, (word) => word[0]);
+// console.log(results1);
+
+// const results2 = map(numbers, (num) => num);
+// console.log(results2)
+
+// assertArraysEqual(results2, [1, 2, 3, 4]);
+
