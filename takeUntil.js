bf7b408d37331ba1c693e08eecc787ca1785f926
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-12 22:12:29 +0000

    Created a function that accepts an array and returns another array containing values up until a value specified by a callback

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// -------------------------------------------------------------------------------------------
+
+const assertArraysEqual = function(actual, expected) {
+  const equalArray = eqArrays(actual, expected);
+  if (equalArray) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+// -------------------------------------------------------------------------------------------
+
+/*
+- Create function that accepts an array and a callback function;
+- 
+*/
+
+
+const takeUntil = function(array, callback) {
+  let output = [];
+  for (let item of array) {
+    if (callback(item)){ 
+      return output;
+    } else {
+      output.push(item);
+    }
+  }
+  return output;
+};
+
+
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+
+// assertArraysEqual(takeUntil(data1, x => x < 0),[1, 2, 5, 7, 2]);
