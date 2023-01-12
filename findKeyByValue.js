dbafe9342f486f5dc7ef3ede2f4b3771af34bea9
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-12 02:59:00 +0000

    Created a function that finds a key within an object according to a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+// -------------------------------------------------------------------------------------------
+
+
+
+const findKeyByValue = function(obj, val) {
+  for (const key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+
+
+
+// const bestTVShowsByGenre = {
+//   sciFi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
