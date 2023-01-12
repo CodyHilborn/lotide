ad939ff23b50f47fe1f588fded9625c2705c58eb
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-12 20:30:13 +0000

    Initial stages of implementing a map function

---
+++ map.js
@@ -0,0 +1,10 @@
+const words = ['welcome', 'to', 'the', 'jungle'];
+
+const map = function() {
+  const results = [];
+  return results;
+};
+
+
+const results = map(words, word => word [0]);
+console.log(results);
