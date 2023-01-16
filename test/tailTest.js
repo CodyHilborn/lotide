99a179203fd5681ab812649c81e013864a498a0f
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-16 20:44:39 +0000

    Refactored tailTest using M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe('#Tail', () => {
+  it('should return [3, 4, 5] for [2, 3, 4, 5]', () => {
+    assert.deepEqual(tail([2, 3, 4, 5]), [3, 4, 5]);
+  });
+  it('should return [] for [2]', () => {
+    assert.deepEqual(tail([2]), []);
+  });
+  it('should return [4] for [3, 4]', () => {
+    assert.deepEqual(tail([3, 4]), [4]);
+  });
+  it('should return an error when comparing [3, 4] with [3]', () => {
+    assert.notDeepEqual(tail([3, 4]), [3]);
+  });
+});
 
-const result = [2, 3, 4, 5];
-assertEqual(tail(result)[0], 3);
-assertEqual(tail(result)[2], 5);
-assertEqual(tail(result)[1], 3);
+
+
+// const result = [2, 3, 4, 5];
+// assertEqual(tail(result)[0], 3);
+// assertEqual(tail(result)[2], 5);
+// assertEqual(tail(result)[1], 3);
