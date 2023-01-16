4ee238760df2c37fa5557ccae08868909f9d293e
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-16 20:51:30 +0000

    Refactored middleTest using M&C, removed dead code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,18 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-console.log(middle([1, 2, 3, 4, 5]), [3]);
-
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [3, 4]);
+describe('#middle', () => {
+  it('should return [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it('should not return [4] for [1, 2, 3, 4, 5]', () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5]), [4]);
+  });
+  it('should return [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it('should not return [1, 2] for [1, 2, 3, 4]', () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [1, 2]);
+  });
+});
 
