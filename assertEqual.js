9832b2d0a1704dabefc03d5499f4248b1b6fd69d
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-16 18:59:19 +0000

    Removed test code and added them to new file in Test directory

--- assertEqual.js
+++ assertEqual.js
@@ -9,13 +9,6 @@ const assertEqual = function(actual, expected) {
 
 
 
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual('key', 'keys');
-// assertEqual('finger', 'finger');
-// assertEqual('11', 11);
-// assertEqual(11, 11);
-
 module.exports = assertEqual;
 
 
