1651b98f1224665176b95430bc147b7407e82535
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-16 19:24:58 +0000

    Minor code cleanup

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
-//   }
-// };
 
 const assertEqual = require('./assertEqual');
 
