dcc2f51f4219a24df6bb4ae8c6d7f75e3522b390
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-16 18:56:25 +0000

    Created a directory to add test files, created files for assertEqualTest and headTest

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual')
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('key', 'keys');
+assertEqual('finger', 'finger');
+assertEqual('11', 11);
+assertEqual(11, 11);
